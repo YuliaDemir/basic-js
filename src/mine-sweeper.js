bca1821ba204bd9bf6a5fca0361b827848cfe324
Yulia  Demir <Nightelfin91@gmail.com>
2024-12-24 17:30:19 +0300

    feat: mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+	let arr = Array.from({ length: matrix.length }, () => Array(matrix[0].length).fill(0));
+	let count = 0;
+	for (let j = 0; j < matrix.length; j++) {
+		for (let i = 0; i < matrix[0].length; i++) {			
+			count = (matrix[j-1]?.[i-1] || 0) + (matrix[j-1]?.[i] || 0) + (matrix[j-1]?.[i+1] || 0) 
+			+ (matrix[j][i-1] || 0) + (matrix[j][i+1] || 0)
+			+ (matrix[j+1]?.[i-1] || 0) + (matrix[j+1]?.[i] || 0) + (matrix[j+1]?.[i+1] || 0);
+			arr[j][i] = count;
+		}
+	}
+	return arr;
 }
 
 module.exports = {
